75b261e4aff72c40db9f3fad6b8aabbc5f9a1aad
jack272hughes <jack.hughes2@sky.uk>
2022-01-17 19:50:12 +0000

    Add CSSMaps integration
    - Added CSSMaps images, css and js
    - Created razor page for the whole map and south-america map

---
+++ Hotel Advisor/Pages/Map/South-America.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel_Advisor.Pages.Map
+{
+    public class South_AmericaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
