47c8985df48f51c23b10f10deca311808c8bc2d2
Jack Hughes <53757917+Jack272Hughes@users.noreply.github.com>
2022-01-21 12:57:21 +0000

    Create ApplicationUser which includes custom fields
    - User now has favourites, reviews and hotels

---
+++ Hotel Advisor/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Hotel_Advisor.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<Hotel> Hotels { get; set; }
+        public ICollection<Favourite> Favourites { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
