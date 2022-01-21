7fc8d972b4d4063bf8d7fcf50668218844b13e8a
Alysse <22044194@bucks.ac.uk>
2022-01-21 12:59:34 +0000

    Add seeded data for countries and cities

---
+++ Hotel Advisor/Areas/Identity/Data/DbInitializer.cs
@@ -0,0 +1,51 @@
+﻿using Hotel_Advisor.Models;
+
+namespace Hotel_Advisor.Areas.Identity.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(Hotel_Advisor.Data.Hotel_AdvisorContext context)
+        {
+            if (context.Continents.Any())
+            {
+                return;
+            }
+
+            var Continents = new Continent[]
+            {
+                new Continent{Name="Europe"},
+                new Continent{Name="Asia"},
+                new Continent{Name="Africa"},
+                new Continent{Name="Oceania"},
+                new Continent{Name="North America"},
+                new Continent{Name="South America"},
+                new Continent{Name="Antartica"}
+            };
+
+            context.Continents.AddRange(Continents);
+            context.SaveChanges();
+
+            var Countries = new Country[]
+                {
+                    new Country{ContinentID=6, Name="Argentina" , CountryCode="AR"},
+                    new Country{ContinentID=6, Name="Bolivia" , CountryCode="BO"},
+                    new Country{ContinentID=6, Name="Brazil" , CountryCode="BR"},
+                    new Country{ContinentID=6, Name="Chile" , CountryCode="CL"},
+                    new Country{ContinentID=6, Name="Colombia" , CountryCode="CO"},
+                    new Country{ContinentID=6, Name="Ecuador" , CountryCode="EC"},
+                    new Country{ContinentID=6, Name="Falkland Islands" , CountryCode="FK"},
+                    new Country{ContinentID=6, Name="French Guiana" , CountryCode="GF"},
+                    new Country{ContinentID=6, Name="Guyana" , CountryCode="GY"},
+                    new Country{ContinentID=6, Name="Peru" , CountryCode="PE"},
+                    new Country{ContinentID=6, Name="Paraguay" , CountryCode="PY"},
+                    new Country{ContinentID=6, Name="Suriname" , CountryCode="SR"},
+                    new Country{ContinentID=6, Name="Uruguay" , CountryCode="UY"},
+                    new Country{ContinentID=6, Name="Venezuela" , CountryCode="VE"},
+
+                };
+
+            context.Countries.AddRange(Countries);
+            context.SaveChanges();
+        }
+    }
+}
--- Hotel Advisor/Migrations/20220121124746_CountryCodeString.Designer.cs
+++ Hotel Advisor/Migrations/20220121124746_CountryCodeString.Designer.cs
@@ -4,6 +4,7 @@ using Hotel_Advisor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hotel_Advisor.Migrations
 {
     [DbContext(typeof(Hotel_AdvisorContext))]
-    partial class Hotel_AdvisorContextModelSnapshot : ModelSnapshot
+    [Migration("20220121124746_CountryCodeString")]
+    partial class CountryCodeString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
