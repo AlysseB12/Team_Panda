978d93e5d713a5c9f875f58526793286f4ee2593
Alysse <22044194@bucks.ac.uk>
2022-01-21 15:54:58 +0000

    Update DbInitializer to seed database, User and Hotel.

--- Hotel Advisor/Areas/Identity/Data/DbInitializer.cs
+++ Hotel Advisor/Areas/Identity/Data/DbInitializer.cs
@@ -46,6 +46,46 @@ namespace Hotel_Advisor.Areas.Identity.Data
 
             context.Countries.AddRange(Countries);
             context.SaveChanges();
+
+            var Users = new ApplicationUser[]
+            {
+                new ApplicationUser
+                {
+                    Id = "a93f6a42-1b45-4e9b-805c-ae73b79d6cd3",
+                    UserName = "alysse@test.co.uk",
+                    NormalizedUserName = "ALYSSE@TEST.CO.UK",   
+                    Email = "alysse@test.co.uk",
+                    NormalizedEmail = "ALYSSE@TEST.CO.UK",
+                    EmailConfirmed = true,
+                    PasswordHash = "AQAAAAEAACcQAAAAEBAZn/GfonIhwSZZ71auZYdYMN4s9XvQ7YebAfA5z0cUxrFqv9I5VBaJt6Qii2Wydg==",
+                    SecurityStamp = "7I5DE4E3QOUWDI6VDWTYC76RXK2VO3GD",
+                    ConcurrencyStamp = "6b6459d7-9dab-480f-a4f1-90a673b33dc7",
+                    PhoneNumber = null,
+                    PhoneNumberConfirmed= false,
+                    TwoFactorEnabled = false,
+                    LockoutEnd = null,
+                    LockoutEnabled = true,
+                    AccessFailedCount = 0
+                }
+            };
+
+            context.Users.AddRange(Users);
+            context.SaveChanges();
+
+            var Hotels = new Hotel[]
+            {
+                new Hotel{CountryID= 3,
+                   UserID= "a93f6a42-1b45-4e9b-805c-ae73b79d6cd3" ,
+                   Name= "Bem-vindo a casa",
+                   Description="Come stay at the best hotel in Brazil, our house is your house so relax and enjoy your stay",
+                   Address="Largo Vespasiano Julio Veppo 77, Center ",
+                   City="Porto Alegre",
+                   Website="www.Bem-vida-Casa.com",
+                   CovidSafety="Masks are mandatory while moving around the hotel, Sanitary stations throughout, Extra routine cleaning"}  
+            };
+
+            context.Hotels.AddRange(Hotels);
+            context.SaveChanges();
         }
     }
 }
