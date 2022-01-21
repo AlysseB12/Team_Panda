23c0923e68f2ec2e22d798f0f18c5a32aa563622
jack272hughes <jack.hughes2@sky.uk>
2022-01-21 17:30:04 +0000

    Add required packages and create basic test

---
+++ Hotel Advisor/Tests/AccountUITests.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace Hotel_Advisor.Tests
+{
+    public class AccountUITests : IDisposable
+    {
+        private readonly IWebDriver driver;
+
+        public AccountUITests() => driver = new ChromeDriver();
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void LoggingInAsExistingUser_LogsIn()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7292/Identity/Account/Login");
+        }
+    }
+}
