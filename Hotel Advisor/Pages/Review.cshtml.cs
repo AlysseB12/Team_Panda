f507c61271182154b6ba0f9dd161779b1f86555a
Alysse <22044194@bucks.ac.uk>
2022-01-24 22:21:31 +0000

    Update Nav and add corresponding pages

---
+++ Hotel Advisor/Pages/Review.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel_Advisor.Pages
+{
+    public class ReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
