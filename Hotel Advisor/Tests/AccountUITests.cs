936e250923e449cd825ffdf48128485dc327b1f2
Alysse <22044194@bucks.ac.uk>
2022-01-25 21:41:07 +0000

    Add login and register automated tests

--- Hotel Advisor/Tests/AccountUITests.cs
+++ Hotel Advisor/Tests/AccountUITests.cs
@@ -18,6 +18,48 @@ namespace Hotel_Advisor.Tests
         public void LoggingInAsExistingUser_LogsIn()
         {
             driver.Navigate().GoToUrl("https://localhost:7292/Identity/Account/Login");
+
+            driver.FindElement(By.Id("Input_Username"))
+                .SendKeys("alysse@test.co.uk");
+
+            driver.FindElement(By.Id("Input_Password"))
+                .SendKeys("Advisor-2012");
+
+            driver.FindElement(By.Id("login-submit"))
+                .Click();
+
+            var welcomeText = driver.FindElement(By.Id("manage")).Text;
+
+            Assert.Equal("Hello alysse@test.co.uk!", welcomeText);
+        }
+
+        [Fact]
+        public void RegisterAsAUser()
+        {
+            var username = Guid.NewGuid();
+
+            driver.Navigate().GoToUrl("https://localhost:7292/Identity/Account/Register");
+
+            driver.FindElement(By.Id("Input_Username"))
+                .SendKeys(username.ToString());
+
+            driver.FindElement(By.Id("Input_Email"))
+                .SendKeys("Jack@test.co.uk");
+
+            driver.FindElement(By.Id("Input_Password"))
+                .SendKeys("Advisor-2022");
+
+            driver.FindElement(By.Id("Input_ConfirmPassword"))
+                .SendKeys("Advisor-2022");
+
+            driver.FindElement(By.Id("registerSubmit"))
+                .Click();
+
+            var welcomeText = driver.FindElement(By.Id("manage")).Text;
+
+            var expectedText = "Hello " + username.ToString() + "!";
+
+            Assert.Equal(expectedText, welcomeText);
         }
     }
 }
