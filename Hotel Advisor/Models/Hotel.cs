ffa829a88ee5733890361150af69edd4a702c4c2
jack272hughes <jack.hughes2@sky.uk>
2022-01-29 12:58:46 +0000

    Update design for create/edit pages

--- Hotel Advisor/Models/Hotel.cs
+++ Hotel Advisor/Models/Hotel.cs
@@ -1,19 +1,31 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hotel_Advisor.Models
 {
     public class Hotel
     {
         public int ID { get; set; }
+        [Required]
         public int CountryID { get; set; }
         public Country? Country { get; set; }
         public string? UserID { get; set; }
         public ApplicationUser? User { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Address { get; set; }
+        [Required]
+        [StringLength(50)]
         public string City { get; set; }
+        [Required]
+        [DataType(DataType.Url)]
         public string Website { get; set; }
+        [Required]
         public string CovidSafety { get; set; }
         public ICollection<Review>? Reviews { get; set; }
 
