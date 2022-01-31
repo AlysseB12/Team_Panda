9dc84c171e4101e7e7160874a68acf8ae2254821
Jack Hughes <53757917+Jack272Hughes@users.noreply.github.com>
2022-01-31 10:15:54 +0000

    Update unformatted buttons and fix viewable hotels
    - User can now only see own hotels on hotels page
    - Updated README with some words

--- Hotel Advisor/Pages/Hotels/Index.cshtml.cs
+++ Hotel Advisor/Pages/Hotels/Index.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Hotel_Advisor.Data;
 using Hotel_Advisor.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Hotel_Advisor
 {
@@ -16,17 +17,22 @@ namespace Hotel_Advisor
     public class IndexModel : PageModel
     {
         private readonly Hotel_Advisor.Data.Hotel_AdvisorContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public IndexModel(Hotel_Advisor.Data.Hotel_AdvisorContext context)
+        public IndexModel(Hotel_Advisor.Data.Hotel_AdvisorContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<Hotel> Hotel { get;set; }
 
         public async Task OnGetAsync()
         {
+            string userId = _userManager.GetUserId(HttpContext.User);
+
             Hotel = await _context.Hotels
+                .Where(h => h.UserID.Equals(userId))
                 .Include(h => h.Country)
                 .Include(h => h.User).ToListAsync();
         }
--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 Team Panda was founded by Jon Jackson who brought everyone together with the vision of creating innovative web apps using Microsoft's dotnet framework. The name "Panda" originates from the animal of the same name and was chosen to raise awareness as they are nearly extinct.
 
-Hello Jack and Jake
-
-Hello both 
-
-Hello mysterious strangers
+This github shows our collaboration in creating the covid aware Hotel Advisor!
